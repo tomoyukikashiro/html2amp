073f126d56a13c177637a02f5e6a5cd5175d9d43
tomoyukikashiro <ksrtmyk@gmail.com>
2018-11-15 21:31:48 +0900

    feat(lib): added main

---
+++ index.js
@@ -0,0 +1,26 @@
+const cheerio = require('cheerio')
+const amp = require('./lib/amp')
+const css = require('./lib/css')
+const img = require('./lib/img')
+const script = require('./lib/script')
+const charset = require('./lib/meta/charset')
+const viewport = require('./lib/meta/viewport')
+const ga = require('./lib/googleanalytics')
+const iframe = require('./lib/iframe')
+const boilerplate = require('./lib/boilerplate')
+
+const html2amp = (html, options = {}) => {
+  let $ = cheerio.load(html)
+  $ = amp($, options)
+  $ = css($, options)
+  $ = img($, options)
+  $ = script($, options)
+  $ = charset($, options)
+  $ = viewport($, options)
+  $ = ga($, options)
+  $ = iframe($, options)
+  $ = boilerplate($, options)
+  return $.html()
+}
+
+module.exports = html2amp
