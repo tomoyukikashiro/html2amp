25df1e8d1369aa368861d62bf664ff0dd0ba411c
tomoyukikashiro <ksrtmyk@gmail.com>
2020-05-03 14:20:40 +0900

    feat(preload): Add feature to remove preload script/fetch

---
+++ test/link/preload.test.js
@@ -0,0 +1,28 @@
+const cheerio = require('cheerio')
+const assert = require('../../test/assert')
+const canonical = require('../../lib/link/preload')
+const htmlFactory = require('../../test/html')
+
+describe('preload', function () {
+  describe('If there is link tag for preloading script', function () {
+    const html = htmlFactory({ head: '<link rel="preload" as="script" href="https://example.com/test.js">' })
+    it('should be removed', function () {
+      const $ = canonical(cheerio.load(html))
+      assert($, htmlFactory({ head: '' }))
+    })
+  })
+  describe('If there is link tag for preloading fetch resource', function () {
+    const html = htmlFactory({ head: '<link rel="preload" as="fetch" href="https://example.com/test.json">' })
+    it('should be removed', function () {
+      const $ = canonical(cheerio.load(html))
+      assert($, htmlFactory({ head: '' }))
+    })
+  })
+  describe('If there is link tag for preloading style', function () {
+    const html = htmlFactory({ head: '<link rel="preload" as="style" href="https://example.com/test.css">' })
+    it('shouldn\'t be removed', function () {
+      const $ = canonical(cheerio.load(html))
+      assert($, htmlFactory({ head: '<link rel="preload" as="style" href="https://example.com/test.css">' }))
+    })
+  })
+})
